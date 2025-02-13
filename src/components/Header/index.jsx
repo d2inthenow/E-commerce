63403a7d5b2aba3b489c44903c1766c972af9147
duong\DUONG <duomg0023@gmail.com>
2025-02-13 21:47:54 +0700

    init project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip"></div>
+
+      <div className="container"></div>
+    </header>
+  );
+};
+
+export default Header;
