20f2c261ded4361b9d6ef6129ef1d9f5e772a41f
duong\DUONG <duomg0023@gmail.com>
2025-02-27 11:00:05 +0700

    update Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -25,14 +25,14 @@ const Header = () => {
       <div className="top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px]">
         <div className="container">
           <div className="flex items-center justify-between">
-            <div className="col1 w-[50%]">
-              <p className="text-[14px] font-bold">
+            <div className="col1 w-[50%] moving-text-container">
+              <p className="text-[14px] font-bold moving-text">
                 Get up to 50% new season styles, limited time only
               </p>
             </div>
 
             <div className="col2 flex items-center justify-end">
-              <ul className="flex items-center gap-2">
+              <ul className="flex items-center gap-4 min-w-50">
                 <li className="list-none">
                   <Link
                     to="/help-center"
--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "../ProductItem/style.css";
 import product from "../../assets/images/ProductItem/product.jpg";
 import product1 from "../../assets/images/ProductItem/product.jpg";
@@ -6,15 +7,19 @@ import product2 from "../../assets/images/ProductItem/product.jpg";
 import product3 from "../../assets/images/ProductItem/product.jpg";
 const ProductItem = () => {
   return (
-    <div className="productIem rounded-md overflow-hidden ">
+    <div className="productItem rounded-md overflow-hidden ">
       <div className="imgWrapper w-[100%] h-[450px] overflow-hidden rounded-md">
         <img src={product} alt="" className="w-full" />
       </div>
 
       <div className="info !py-3">
-        <h6 className="text-[14px]">Dong Duong</h6>
-        <h3 className="text-[15px] title !mt-2 font-[500]">
-          Panda jacker with jean{" "}
+        <h6 className="text-[14px]">
+          <Link to="/" className="link">
+            Dong Duong
+          </Link>
+        </h6>
+        <h3 className="text-[15px] title !mt-2 font-[500] text-[rgba(0,0,0,0.8)]">
+          Panda jacket with jean pants and T-shirt
         </h3>
       </div>
     </div>
